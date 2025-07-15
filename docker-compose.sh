85bba0935faf15e9f9464f32536c59aacdcf7f2f
MUSTAFA SHAIK <106308110+devops0014@users.noreply.github.com>
2025-07-15 23:02:16 +0530

    Update docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,5 +1,17 @@
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-ls /usr/local/bin/
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+vim .bashrc
+export PATH=$PATH:/usr/local/bin/
+source .bashrc
+
+# Download the current stable release of Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# Apply executable permissions
 sudo chmod +x /usr/local/bin/docker-compose
-docker-compose version
+# Create a symbolic link
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# Install using pip (Python package manager)
+sudo yum install -y python3-pip
+sudo pip3 install docker-compose
+
+#check version
+docker-compose --version
