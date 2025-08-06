228fec782d6bf4285359b14c8c44a8fb273cf518
MUSTAFA SHAIK <106308110+devops0014@users.noreply.github.com>
2025-08-06 23:46:53 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,16 +1,16 @@
-#STEP-1: INSTALLING GIT 
-yum install git  -y
-
-#STEP-2: GETTING THE REPO (jenkins.io --> download -- > redhat)
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-
-#STEP-3: DOWNLOAD JAVA11 AND JENKINS
-yum install java-17-amazon-corretto -y
-yum install jenkins -y
-
-#STEP-4: RESTARTING JENKINS (when we download service it will on stopped state)
-systemctl start jenkins.service
-systemctl enable jenkins.service
-systemctl status jenkins.service
+sudo yum upgrade
+sudo yum install java-17-amazon-corretto -y
+sudo yum install jenkins git -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+sudo mkdir -p /var/tmp_disk
+sudo chmod 1777 /var/tmp_disk
+sudo mount --bind /var/tmp_disk /tmp
+echo '/var/tmp_disk /tmp none bind 0 0' | sudo tee -a /etc/fstab
+sudo systemctl mask tmp.mount
+df -h /tmp
+sudo systemctl restart jenkins
