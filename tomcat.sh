0df1a4c95701e901a2ac9393d349333bba702a7f
yukthasree4 <107660159+yukthasree4@users.noreply.github.com>
2025-08-07 19:54:15 +0530

    Update tomcat.sh
    107 to 108 version

--- tomcat.sh
+++ tomcat.sh
@@ -1,11 +1,11 @@
 yum install java-17-amazon-corretto -y
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.107/bin/apache-tomcat-9.0.107.tar.gz
-tar -zxvf apache-tomcat-9.0.107.tar.gz
-sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.107/conf/tomcat-users.xml
-sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.107/conf/tomcat-users.xml
-sed -i '58  a\<user username="tomcat" password="admin@123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.107/conf/tomcat-users.xml
-sed -i '59  a\</tomcat-users>' apache-tomcat-9.0.107/conf/tomcat-users.xml
-sed -i '56d' apache-tomcat-9.0.107/conf/tomcat-users.xml
-sed -i '21d' apache-tomcat-9.0.107/webapps/manager/META-INF/context.xml
-sed -i '22d'  apache-tomcat-9.0.107/webapps/manager/META-INF/context.xml
-sh apache-tomcat-9.0.107/bin/startup.sh
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.108/bin/apache-tomcat-9.0.108.tar.gz
+tar -zxvf apache-tomcat-9.0.108.tar.gz
+sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.108/conf/tomcat-users.xml
+sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.108/conf/tomcat-users.xml
+sed -i '58  a\<user username="tomcat" password="admin@123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.108/conf/tomcat-users.xml
+sed -i '59  a\</tomcat-users>' apache-tomcat-9.0.108/conf/tomcat-users.xml
+sed -i '56d' apache-tomcat-9.0.108/conf/tomcat-users.xml
+sed -i '21d' apache-tomcat-9.0.108/webapps/manager/META-INF/context.xml
+sed -i '22d'  apache-tomcat-9.0.108/webapps/manager/META-INF/context.xml
+sh apache-tomcat-9.0.108/bin/startup.sh
